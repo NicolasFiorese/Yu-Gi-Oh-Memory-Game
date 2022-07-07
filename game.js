3a7df384ecef1e8050b8581a38fa35d74bacef84
Nicolas Fiorese <103607839+NicolasFiorese@users.noreply.github.com>
2022-07-07 20:05:32 -0300

    Update game.js

--- game.js
+++ game.js
@@ -78,7 +78,7 @@ const createCard = character => {
   const front = createElement('div', 'face front')
   const back = createElement('div', 'face back')
 
-  front.style.backgroundImage = `url('../images/${character}.png')`
+  front.style.backgroundImage = `url('images/${character}.png')`
 
   card.appendChild(front)
   card.appendChild(back)
