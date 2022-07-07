af6e8bc436bf8ff158176427a438543eeb7bf158
Nicolas Fiorese <103607839+NicolasFiorese@users.noreply.github.com>
2022-07-07 19:47:52 -0300

    Add files via upload

---
+++ js/login.js
@@ -0,0 +1,20 @@
+const input = document.querySelector('.login-input')
+const button = document.querySelector('.login-button')
+const form = document.querySelector('.login-form')
+const validateInput = ({ target }) => {
+  if (target.value.length > 1) {
+    button.removeAttribute('disabled')
+  } else {
+    button.setAttribute('disabled', '')
+  }
+}
+
+const handleSubmit = event => {
+  event.preventDefault()
+
+  localStorage.setItem('player', input.value)
+  window.location = 'pages/game.html'
+}
+
+input.addEventListener('input', validateInput)
+form.addEventListener('submit', handleSubmit)
